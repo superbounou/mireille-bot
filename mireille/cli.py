491f4c05308f7eaedf65db3042deec153ffd7a9e
nicolas <nicolas.bounoughaz@gmail.com>
2018-04-17 22:00:17 +0200

    add record command

--- mireille/cli.py
+++ mireille/cli.py
@@ -3,21 +3,23 @@ import json
 import logging
 import sys
 import ConfigParser
+from .audio import Audio
 
 LOGGER = logging.getLogger(__name__)
 CONFIG = ConfigParser.ConfigParser()
 
 #pylint: disable=invalid-name
-class ExampleStage(object): # pylint: disable=too-few-public-methods
+class AudioStage(object): # pylint: disable=too-few-public-methods
     """This class manage CLI interactions"""
     @staticmethod
-    def foo(): # pylint: disable=invalid-name
+    def record(): # pylint: disable=invalid-name
         """FOO"""
-        print("foo")
+        audio = Audio()
+        audio.record_to_file('test.wav')
 
 
 class Cli(object):# pylint: disable=too-few-public-methods
     """CLI class"""
     def __init__(self):
         """CLI constructor"""
-        self.stage = ExampleStage()
+        self.audio = AudioStage()
